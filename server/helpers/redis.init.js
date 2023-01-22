cb166574c18d1e398c6ac9b4a5fc446bdbb0730c
Kiptoo Biwot <elviskb@gmail.com>
2023-01-22 15:09:40 +0300

    updated redis cloud db

--- server/helpers/redis.init.js
+++ server/helpers/redis.init.js
@@ -1,10 +1,17 @@
-const redis = require("redis");
+const Redis = require('ioredis');
+// const fs = require('fs');
 
-const client = redis.createClient({
+const client = new Redis({
   port: process.env.REDIS_PORT || 6379,
   host: process.env.REDIS_HOST || "127.0.0.1",
   password: process.env.REDIS_PASSWORD || null
 });
+
+// const client = redis.createClient({
+//   port: process.env.REDIS_PORT || 6379,
+//   host: process.env.REDIS_HOST || "127.0.0.1",
+//   password: process.env.REDIS_PASSWORD || null
+// });
 
 client.on("connect", () => {
   console.log("Client connected to redis");
